c788f6f5b52ef5d1f7f24c2e48ac729f5ca702f6
cjjland76 <cjjland76@yahoo.co.jp>
2020-09-14 17:23:09 +0900

    nickname追加

--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,12 @@
 ActiveRecord::Schema.define(version: 2020_09_14_061240) do
 
   create_table "users", force: :cascade do |t|
+    t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "nickname"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
